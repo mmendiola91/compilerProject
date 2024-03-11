8686fdd2ca957957a0840a8f56da498cca030504
mmendiola2824 <mendiola.matthew.91@gmail.com>
2024-03-11 12:04:19 -0500

    added program to git repository
    migrated the working program into the Git Repository

---
+++ compiler/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+using namespace std;
+#include "LexAnalyzer.h"
+#include "SyntaxAnalyzer.h"
+
+int main() {
+    /// Lexeme Analyzer
+    ifstream lexemeFile("lexemes.txt");
+    if (!lexemeFile){
+        cout << "can't find lexeme file" << endl;
+        exit(-1);
+    }
+    ifstream sourceFile("source.txt");
+    if (!sourceFile){
+        cout << "can't find source file" << endl;
+        exit(-1);
+    }
+    /// Syntax Analyzer
+    ifstream infile("codelexemes.txt");
+    if (!infile){
+        cout << "error opening lexemes.txt file" << endl;
+        exit(-1);
+    }
+
+
+    ofstream outFile("output.txt");
+    LexAnalyzer lexAn(lexemeFile);
+    //lexAn.printMap();
+    if (!lexAn.scanFile(sourceFile, outFile)){
+        cout << "error in source file" << endl;
+    }
+    lexAn.printVectors(outFile);
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    SyntaxAnalyzer sa(infile);
+    sa.parse();
+    return 0;
+}
